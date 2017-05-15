e9154a541533ffdd6d36cc5814d5811df89e2af6
Martin Merino <maedvim@gmail.com>
2017-05-15 20:59:51 +0000

    planes home page

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -10,24 +10,24 @@ var barValue = function  barValue(){
        
        switch (value) {
            case "0":
-               $(".barValue").text("$0");
-               $(".monto").text("$0");
+               $(".barValue").text("$ 0");
+               $(".monto").text("$ 0");
                break;
            case "25":
-                $(".barValue").text("$6,000");
-                $(".monto").text("$60,000");
+                $(".barValue").text("$ 6,000");
+                $(".monto").text("$ 30,000");
                break;
            case "50":
-               $(".barValue").text("$20,000");
-               $(".monto").text("$200,000");
+               $(".barValue").text("$ 20,000");
+               $(".monto").text("$ 50,000");
                break;
            case "75":
-           $(".barValue").text("$50,000");
-           $(".monto").text("$500,000");
+           $(".barValue").text("$ 50,000");
+           $(".monto").text("$ 250,000");
                break;
            case "100":
-               $(".barValue").text("$   100,000");
-               $(".monto").text("$  1'000,000");
+               $(".barValue").text("$ 100,000");
+               $(".monto").text("$ 500,000");
                break;
                
            default:
