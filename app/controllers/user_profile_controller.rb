014bbd3a27dfe6ff599ee41aec29aecf5e5195ab
Martin Merino <maedvim@gmail.com>
2017-05-18 17:56:17 +0000

    rutas y vistas de user_profile

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -1,2 +1,17 @@
 class UserProfileController < ApplicationController
+    def index
+        render 'principal'
+    end
+    
+    def ajustes
+        render 'ajustes'
+    end
+    
+    def validacion
+        render 'validacion'
+    end
+    
+    def ayuda
+        render 'ayuda'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   get 'welcome/terminos'
   get 'user_form_firebase/user_form'
   devise_for :users
+  
+  get  'user_profile/index'
+  get 'user_profile/ajustes'
+  get 'user_profile/validacion'
+  get 'user_profile/ayuda'
+  
   resources :user_profile
   resources :user_settings
 end
