85b4e7d8024c4d36fbf9a78ad0bb7a7bd26aa238
Hugo Gutierrez <arnoldohugo@gmail.com>
2017-06-06 17:57:09 +0000

    formulario de firebase data_user

--- app/assets/javascripts/user_form_firebase.js
+++ app/assets/javascripts/user_form_firebase.js
@@ -1,49 +1,116 @@
 $(document).ready(function(){
-   $("#dataUser").submit(function(){
-        dataUser();
-        return false;
+  $(".next-form").on("click", function(){
+      var op = $(this).attr("alt");
+      switch (op) {
+          case '1':
+              $(".1").fadeOut('fast');
+              $(".2").fadeIn("slow");
+              break;
+          case '2':
+              $(".2").fadeOut('fast');
+              $(".3").fadeIn('slow');
+              break;
+          
+          default:
+              // code
+      }
+   });
+   
+   $(".atras").on("click", function(){
+      var op2 = $(this).attr("alt");
+      switch (op2) {
+          case '3':
+              $(".3").fadeOut("fast");
+              $(".2").fadeIn("slow");
+              break;
+          case '2':
+              $(".2").fadeOut("fast");
+              $(".1").fadeIn('slow');
+          break;
+          default:
+              // code
+      }
    });
 });
 
 var dataUser = function dataUser() {
-    var terminos;
-    var contrato;
+    var AP;
+    var CT;
     var id = $("#id_user").val();
     var nombre = $("#nombre").val();
     var apellidoP = $("#apellidoP").val();
     var apellidoM = $("#apellidoM").val();
-    var RFC = $("#RFC").val();
-    var identifiacion = $("#identifiacion").val();
-    var ciudad = $("#Ciudad").val();
-    var colonia = $("#Colonia").val();
-    var calle = $("#calle").val();
-    var CP = $("#CP").val();
-    var TP = $("#TP").val();
-    var CEL = $("#Cel").val();
     var CURP = $("#CURP").val();
-    var Monto = $('input:radio[name=monto]:checked').val();
-    console.log(Monto);
-        
+    var RFC = $("#RFC").val();
+    var FechaAño = $("#FechaAño").val();
+    var FechaMes = $("#FechaMes").val();
+    var FechaDia = $("#FechaDia").val();
+    var Ide = $("#Ide").val();
+    var Identificacion = $("#Identificacion").val();
+    
+    var Estado = $("#Estado").val();
+    var Ciudad = $("#Ciudad").val();
+    var Calle = $("#Calle").val();
+    var Colonia = $("#Colonia").val();
+    var CP = $("#CP").val();
+    var TF = $("#TF").val();
+    var TM = $("#TM").val();
+    
+    
+
+    // Concatenar Fecha
+    var Fecha = FechaAño+"-"+FechaMes+"-"+FechaDia;
+    
+    
+    //Banca
+    var CB = $("#CB").val();
+    var B1 = $("#B1").val();
+    var B2 = $("#B2").val();
+    var Plan = $("#Plan").val();
+    
+  
         if ($('input:checkbox[name=t1]:checked').val() == "true") {
-            terminos = true;
+            AP = true;
         }else{
-            terminos = false;
+            AP = false;
         }
 
         if ($('input:checkbox[name=c1]:checked').val() == "true") {
-            contrato = true;
+            CT = true;
         }else{
-            contrato = false;
+            CT = false;
         }
         
      
-    if (terminos && contrato) {
-        writeUserData(id,nombre,apellidoP,apellidoM,RFC,identifiacion,ciudad,colonia,calle,CP,TP,CEL,CURP,Monto);
+    if (AP && CT) {
+        writeUserData(
+              id,
+        nombre,
+        apellidoP,
+        apellidoM,
+        CURP,
+        RFC,
+        Fecha,
+        Ide,
+        Identificacion,
+        Estado,
+        Ciudad,
+        Calle,
+        Colonia,
+        CP,
+        TF,
+        TM,
+        CB,
+        B1,
+        B2,
+        Plan
+            );
                swal(
               'Buen Trabajo!',
               'Bienvenido a Fidei',
               'success'
                 )
+                
         return false;
       
     }else{
@@ -53,22 +120,52 @@ var dataUser = function dataUser() {
     
 }
 
-var writeUserData = function writeUserData(id,nombre,apellidoP,apellidoM,RFC,identifiacion,ciudad,colonia,calle,CP,TP,CEL,CURP,Monto) {
+var writeUserData = function writeUserData(
+        id,
+        nombre,
+        apellidoP,
+        apellidoM,
+        CURP,
+        RFC,
+        Fecha,
+        Ide,
+        Identificacion,
+        Estado,
+        Ciudad,
+        Calle,
+        Colonia,
+        CP,
+        TF,
+        TM,
+        CB,
+        B1,
+        B2,
+        Plan
+
+) {
     
   firebase.database().ref('users/' + id).set({
     nombre: nombre,
     apellidoP: apellidoP,
     apellidoM: apellidoM,
-    RFC: RFC,
-    identifiacion: identifiacion,
-    ciudad: ciudad,
-    colonia: colonia,
-    calle: calle,
-    CP: CP,
-    TP: TP,
-    CEL: CEL,
     CURP: CURP,
-    Monto: Monto
+    RFC: RFC,
+    Fecha: Fecha,
+    Ide: Ide,
+    Identificacion: Identificacion,
+    Ciudad: Ciudad,
+    Colonia: Colonia,
+    Calle: Calle,
+    CP: CP,
+    TF: TF,
+    TM: TM,
+    CB: CB,
+    B1: B1,
+    B2: B2,
+    Plan: Plan,
    
   });
 }
+
+
+
