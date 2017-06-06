c52dfd46046e7f088af100f73fc25730c71dfebc
Hugo Gutierrez <arnoldohugo@gmail.com>
2017-06-06 23:32:35 +0000

    get names, and email user_profile

--- app/assets/javascripts/data_user.js
+++ app/assets/javascripts/data_user.js
@@ -6,12 +6,18 @@ var firebase_data_user = function firebase_data_user(){
 	var user = $("#id_user_to_data").val();
 	var commentsRef = firebase.database().ref('users/' + user);
 	commentsRef.on('value', function(data) {
-		showdata(data.val().nombre, data.val().CEL);
+		showdata(data.val());
 	});
 	
 	
 }
-var showdata = function(nombre,cel){
-	console.log(nombre);
-	console.log(cel);
+var showdata = function(data){
+	console.log(data.nombre);
+	$("#nombres").html(data.nombre);
+	$("#apellidos").html(data.apellidoP+" "+data.apellidoM);
+	firebase.auth().onAuthStateChanged(function(user) {
+		  if (user) {
+		  	$("#emails").html(user.email);
+		  }
+	});
 }
--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -1,4 +1,10 @@
 class UserProfileController < ApplicationController
+    before_action :user, only: [:index,:ajustes,:validacion,:ayuda]
+    
+    def user
+         @id = current_user.id
+    end
+    
     def index
         render 'principal'
     end
