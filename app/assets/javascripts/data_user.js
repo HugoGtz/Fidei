0a7cdefc556b60c58ce9e37a50d313f490063c26
Hugo Gutierrez <arnoldohugo@gmail.com>
2017-06-07 18:49:13 +0000

    show data into ayuda_user

--- app/assets/javascripts/data_user.js
+++ app/assets/javascripts/data_user.js
@@ -14,8 +14,9 @@ var firebase_data_user = function firebase_data_user(){
 }
 var showdata = function(data){
 	console.log(data.nombre);
-	$("#nombres").html(data.nombre);
+	$("#nombres").html(data.nombre+" ");
 	$("#Nombre_u").val(data.nombre);
+	$("#nombres_ua").val(data.nombre+" "+data.apellidoP+" "+data.apellidoM)
 	$("#apellidos").html(data.apellidoP+" "+data.apellidoM);
 	$("#AP_u").val(data.apellidoP);
 	$("#AM_u").val(data.apellidoM);
@@ -42,6 +43,7 @@ var showdata = function(data){
 	firebase.auth().onAuthStateChanged(function(user) {
 		  if (user) {
 		  	$("#emails").html(user.email);
+		  	$("#email_u").val(user.email);
 		  }
 	});
 }
