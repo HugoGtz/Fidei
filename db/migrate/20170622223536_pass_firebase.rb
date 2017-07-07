b653acd472c23cad9939b6d911a54ac7640ed2bc
Hugo Gutierrez <arnoldohugo@gmail.com>
2017-07-07 22:55:02 +0000

    rama alterna de pruebas para Gpass and Gpdf

--- db/migrate/20170622223536_pass_firebase.rb
+++
@@ -1,5 +0,0 @@
-class PassFirebase < ActiveRecord::Migration
-  def change
-    add_column :users, :pass_firebase, :string, default: "false"
-  end
-end
--- db/migrate/20170622224247_posicion.rb
+++
@@ -1,8 +0,0 @@
-class Posicion < ActiveRecord::Migration
-  def change
-    add_column :p1s, :posicion, :integer, default: "null"
-    add_column :p2s, :posicion, :integer, default: "null"
-    add_column :p3s, :posicion, :integer, default: "null"
-    add_column :p4s, :posicion, :integer, default: "null"
-  end
-end
