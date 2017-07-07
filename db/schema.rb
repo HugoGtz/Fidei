78a5b808ae56d5898cb93647c29a5e56ca3b740c
Hugo Gutierrez <arnoldohugo@gmail.com>
2017-07-07 23:31:54 +0000

    fixeo de layouts

--- db/schema.rb
+++ db/schema.rb
@@ -63,10 +63,6 @@ ActiveRecord::Schema.define(version: 20170704213856) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "superadmin_role",        default: false
