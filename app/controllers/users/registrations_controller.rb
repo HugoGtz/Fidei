9b498bcd746bf2b9f20d70dfc94659569c411d98
Hugo Gutierrez <arnoldohugo@gmail.com>
2017-07-10 05:11:16 +0000

    delete defs

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
   def create
-    pass = gPass
-    
     user = User.new((user_params).merge(pass_firebase: params[:user][:password]))
     if user.save
       sign_in(user)
@@ -14,21 +12,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   protected
-       
-       def token(pass)
-        user = User.find_by(current_user.id)
-        user.pass_firebase = pass.to_str
-        user.save
-       end
       def user_params
         params.require(:user).permit(:email, :password, :password_confirmation)
-      end
-      
-      def gPass
-      g = ('a'..'z').to_a
-      #=> ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-      
-     pass = 10.times.map { g.sample }.join
-      return pass
       end
 end
